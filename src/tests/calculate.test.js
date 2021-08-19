b13d818382f9c7e5ca5a5376330365fe846a482f
Sebastián M <smunozmo@gmail.com>
2021-08-19 11:16:54 -0500

    Add caclulate and snapshot tests.

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,3 +1,24 @@
-import operate from '../components/logic/operator';
+import calculate from '../components/logic/calculate';
 
- 
+const obj = {
+    total: 2,
+    next: 10,
+    operation: '+',
+}
+
+test('calculate snapshot test', () => {
+    const calc = calculate(obj, '=');
+    expect(calc).toMatchSnapshot();
+});
+
+test('= button returns total', () => {
+    expect(calculate(obj, '=')).toStrictEqual({"next": null, "operation": null, "total": "12"});
+});
+
+test('AC button returns null', () => {
+    expect(calculate(obj, 'AC')).toStrictEqual({"next": null, "operation": null, "total": null});
+});
+
+test('x button returns operation: "x"', () => {
+    expect(calculate(obj, 'x')).toStrictEqual({"next": null, "operation": "x", "total": "12"});
+});
--- src/tests/operator.test.js
+++ src/tests/operator.test.js
@@ -1,19 +1,22 @@
 import operate from '../components/logic/operator';
 
- test('test sum', () => {
+ test('test sum of 2 + 2', () => {
      expect(operate(2, 2, '+')).toBe('4');
  })
 
- test('test rest', () => {
+ test('test subtraction of 9 - 5', () => {
      expect(operate(9, 5, '-')).toBe('4');
  })
 
- test('test multiply', () => {
+ test('test multiplication of 11 x 2', () => {
      expect(operate(11, 2, 'x')).toBe('22');
  })
 
- test('test division', () => {
+ test('test division of 25 / 5', () => {
     expect(operate(25, 5, '÷')).toBe('5');
  })
- 
 
+test('operate snapshot test', () => {
+    const calc = operate(1, 1, '+');
+    expect(calc).toMatchSnapshot();
+});
